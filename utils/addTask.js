862f5427f6e479cf2114108897f013084a2f49d6
Jack Davenport <jckwdvnprt@gmail.com>
2024-05-20 09:48:23 +0100

    Added test functionality

---
+++ utils/addTask.js
@@ -0,0 +1,61 @@
+const { MongoClient } = require("mongodb");
+
+// Connection string with the password
+const uri =
+  "mongodb+srv://admin:tetriplan@cluster0.spo41rp.mongodb.net/tetriplan-tasks";
+
+const client = new MongoClient(uri);
+
+async function addTask(
+  taskID,
+  userID,
+  taskName,
+  description,
+  category,
+  date,
+  startTime,
+  endTime,
+  duration,
+  completionStatus,
+  label,
+  priority
+) {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB Atlas");
+
+    const database = client.db();
+    const collection = database.collection("tetriplan-tasks");
+
+    const task = {
+      taskID: taskID,
+      userID: userID,
+      taskName: taskName,
+      description: description,
+      category: category,
+      date: date,
+      startTime: startTime,
+      endTime: endTime,
+      duration: duration,
+      completionStatus: completionStatus,
+      label: label,
+      priority: priority,
+    };
+
+    // Insert the new task document
+    const result = await collection.insertOne(task);
+    console.log(
+      `New task inserted with the following id: ${result.insertedId}`
+    );
+
+    const query = { taskID: taskID };
+    const findResult = await collection.findOne(query);
+    console.log("Found task:", findResult);
+
+    return findResult;
+  } finally {
+    await client.close();
+  }
+}
+
+module.exports = addTask;
